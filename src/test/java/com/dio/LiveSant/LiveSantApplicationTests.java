64eb5c8571e9d61d4f6d947bcedb7614dfff85e5
Pedro Rocha <pedrohrrf17@gmail.com>
2021-08-04 15:28:21 -0300

    Finished project

---
+++ src/test/java/com/dio/LiveSant/LiveSantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.LiveSant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveSantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
